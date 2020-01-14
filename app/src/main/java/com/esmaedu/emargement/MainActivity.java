7143ea9e58f99d9afdbaecad8052104768d92700
Yozan Adriel <lukadriel@gmail.com>
2020-01-14 14:42:56 +0000

    Adriel

--- app/src/main/java/com/esmaedu/emargement/MainActivity.java
+++ app/src/main/java/com/esmaedu/emargement/MainActivity.java
@@ -15,6 +15,7 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
 
     // Dylan
+    //Adriel
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
