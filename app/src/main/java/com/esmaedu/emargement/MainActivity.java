d596dfa3bb21d8ba3536a84019a0de993c3bc64d
dylanmouahet <47145599+dylanmouahet@users.noreply.github.com>
2020-01-14 14:54:00 +0100

    Update MainActivity.java

--- app/src/main/java/com/esmaedu/emargement/MainActivity.java
+++ app/src/main/java/com/esmaedu/emargement/MainActivity.java
@@ -14,6 +14,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Dylan
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
