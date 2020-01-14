ae4dd71526cce01421e47b312fbb8fd979918f93
Yozan Adriel <lukadriel@gmail.com>
2020-01-14 12:46:45 +0000

    initial commit

---
+++ app/src/main/java/com/esmaedu/emargement/activity/EnregistrementActivity.java
@@ -0,0 +1,16 @@
+package com.esmaedu.emargement.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.esmaedu.emargement.R;
+
+public class EnregistrementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enregistrement);
+    }
+}
---
+++ app/src/main/java/com/esmaedu/emargement/activity/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.esmaedu.emargement.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.esmaedu.emargement.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
